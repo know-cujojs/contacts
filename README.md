16c502a69fdf90237cd722114bec763e910b4fda
Craig Walls <cwalls@vmware.com>
2013-04-11 10:28:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# cujo-contacts
+
+This is a sample app developed using [cujo.js](http://cujojs.com). It was created
+starting with the [quickstart](https://github.com/cujojs/quickstart) project.
+
+## How to get started
+
+1. `git clone https://github.com/know-cujojs/contacts.git`
+1. `cd contacts`
+1. `npm install`
+1. `npm start`
+1. Open http://localhost:8000/ in your browser
---
+++ app/main.js
@@ -0,0 +1,129 @@
+define({
+
+	root: { $ref: 'dom!contactsApp' },
+	
+	theme: { module: 'theme/basic.css' },
+
+	//
+	// VIEWS
+	//
+	headerView: {
+		render: {
+			template: { module: 'text!app/header/template.html' },
+			replace: { module: 'i18n!app/header/strings' },
+			css: { module: 'css!app/header/style.css' }
+		},
+		insert: { first: 'root' }
+	},
+
+	contactsContainer: { $ref: 'dom.first!.contacts-view-container', at: 'root' },
+
+	listView: {
+		render: {
+			template: {module: 'text!app/list/template.html' },
+			css: { module: 'css!app/list/style.css' }
+		},
+		insert: { first: 'contactsContainer' },
+		bind: {
+			to: { $ref: 'contacts' },
+			comparator: { module: 'app/list/compareByLastFirst' },
+			bindings: {
+				firstName: '.first-name',
+				lastName: '.last-name'
+			}
+		}
+	},
+
+	editView: {
+		render: {
+			template: { module: 'text!app/edit/template.html' },
+			replace: { module: 'i18n!app/edit/strings' },
+			css: { module: 'css!app/edit/style.css' }
+		},
+		insert: { last: 'contactsContainer' }
+	},
+
+	editForm: {
+		element: { $ref: 'dom.first!form', at: 'editView' },
+		connect: { 
+			'contacts.onUpdate': 'reset'
+		}
+	},
+	
+	footerView: {
+		render: {
+			template: { module: 'text!app/footer/template.html' },
+			replace: { module: 'i18n!app/footer/strings.js' },
+			css: { module: 'css!app/footer/style.css' }
+		},
+		insert: { last: 'root' }
+	},
+	
+	//
+	// CONTROLLER
+	//
+	controller: {
+		create: {
+			module: 'app/controller',
+			args: { $ref: 'editForm' }
+		},
+		on: {
+			editForm: {
+				'submit' : 'form.getValues | contacts.update'
+			},
+			listView: {
+				'click:.contact':'contacts.edit'
+			}
+		},
+		connect: {
+			'contacts.onEdit': 'editContact'
+		}
+	},
+	
+	//
+	// COLA
+	//
+	contacts: {
+		create: {
+			module: 'cola/Collection',
+			args: {
+				strategyOptions: {
+					validator: { module: 'app/edit/validateContact' }
+				}
+			}
+		},
+		before: {
+			add: 'cleanContact | generateMetadata',
+			update: 'cleanContact | generateMetadata'
+		}
+	},
+
+	contactStore: {
+		create: {
+			module: 'cola/adapter/LocalStorage',
+			args: 'contacts-demo'
+		},
+		bind: {
+			to: { $ref: 'contacts' }
+		}
+	},
+	
+	//
+	// HELPERS
+	//
+	form: { module: 'cola/dom/form' },
+	cleanContact: { module: 'app/contacts/cleanContact' },
+	generateMetadata: { module: 'app/contacts/generateMetadata' },
+
+
+	// Wire.js plugins
+	plugins: [
+		{ module: 'wire/dom', classes: { init: 'loading' } },
+		{ module: 'wire/dom/render' },
+		{ module: 'wire/on' },
+		{ module: 'wire/aop' },
+		{ module: 'wire/connect' },
+		{ module: 'cola' }
+		
+	]
+});
